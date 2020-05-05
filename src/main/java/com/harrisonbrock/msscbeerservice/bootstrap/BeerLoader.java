75770222506ce29fe162eb0f86c8d2de6e05d607
Harrison Brock <harrison@harrisonbrock.com>
2020-05-05 12:45:45 -0400

    Added boot strap loader

---
+++ src/main/java/com/harrisonbrock/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package com.harrisonbrock.msscbeerservice.bootstrap;
+
+import com.harrisonbrock.msscbeerservice.domain.Beer;
+import com.harrisonbrock.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeer();
+    }
+
+    private void loadBeer() {
+
+        if (beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(45465554545L)
+                    .price(new BigDecimal(12.95))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(45465555545L)
+                    .price(new BigDecimal(11.95))
+                    .build());
+        }
+    }
+}
