3c99475e2cb39e18a106de8e04b61c5fe43d51aa
Harrison Brock <harrison@harrisonbrock.com>
2020-05-05 12:33:33 -0400

    Added Beer Repository

---
+++ src/main/java/com/harrisonbrock/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.harrisonbrock.msscbeerservice.repositories;
+
+import com.harrisonbrock.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
